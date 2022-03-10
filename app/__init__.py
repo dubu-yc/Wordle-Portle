1a5570508eeed8c9c1a5fe7319fff278fa789e7d
Rayat Roy <57008685+microbe73@users.noreply.github.com>
2022-03-10 08:50:27 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
 from flask import session
 from flask import request, render_template, Flask, redirect
 import html
+import db
 app = Flask(__name__)
 debug = True
 
 
 @app.route("/", methods=["GET","POST"])
 def home():
+    if 'username' in request.form.keys() and request.form.get('username') != "":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db.authenticate(username,password)
 	return render_template("login.html")
-
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if 'username' in request.form.keys() and request.form.get('username') != "":
+        #store form information
+         username = request.form.get('username')
+         password = request.form.get('password')
+         confirm = request.form.get('cpassword')	
+    return render_template("register.html")
 app.run()

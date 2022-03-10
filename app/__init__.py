9ade5ad5d59e0ef29aa1f1e6910eadbd3e0d8bcf
Rayat Roy <57008685+microbe73@users.noreply.github.com>
2022-03-10 08:59:50 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,17 +8,19 @@ debug = True
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    if 'username' in request.form.keys() and request.form.get('username') != "":
+    if('username' in request.form.keys() and request.form.get('username') != ""):
         username = request.form.get('username')
         password = request.form.get('password')
         db.authenticate(username,password)
-	return render_template("login.html")
+    return render_template("login.html")
+
 @app.route("/register", methods=["GET","POST"])
 def register():
-    if 'username' in request.form.keys() and request.form.get('username') != "":
-        #store form information
+    if('username' in request.form.keys() and request.form.get('username') != ""):
         username = request.form.get('username')
         password = request.form.get('password')
-        confirm = request.form.get('cpassword')	
+        confirm = request.form.get('cpassword')
+        if(password == cpassword and password != ''):
+            db.create_user(username,password)	
     return render_template("register.html")
 app.run()

2bafdb5a61dbd718bc74e273b19adab1fe756ca1
rit roy <ritroy@rits-MacBook-Air.local>
2022-03-14 23:25:16 -0400

    wording on workle logix

--- app/templates/word.js
+++ app/templates/word.js
@@ -14,4 +14,19 @@ fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true&letters=5", {
 });
 }
 let c = get_word();
-console.log(c)
+console.log(c);
+let word = "queen";
+let word_chars = word.split('');
+let guess = "opens";
+let guess_chars = word.split('');
+let doubles = ['','']
+for (var i = 0; i < word_chars.length; i++) {
+  for (var j = i; j < word_chars.length; j++) {
+    if (doubles[0] == '' && word_chars[i] == word_chars[j]) {
+      doubles[0] = word_chars[j];
+    } 
+    else if (word_chars[i] == word_chars[j]){
+      doubles[1] = word_chars[j];
+    }
+  }
+}

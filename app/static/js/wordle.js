e7e00867615e544fc7589d30604db6d6ee9cc810
dubu-yc <ichang20@stuy.edu>
2022-03-18 09:26:41 -0400

    edit

--- app/static/js/wordle.js
+++ app/static/js/wordle.js
@@ -10,22 +10,6 @@ let py_data = fetch('/wordget')
   word = data;
 })
 
-function initBoard() {
-    let board = document.getElementById("wordle-board");
-    for (let i = 0; i < num_guesses; i++) {
-        let row = document.createElement("div")
-        row.className = "letter-row"
-        for (let j = 0; j < 5; j++) {
-            let box = document.createElement("div")
-            box.className = "letter-box"
-            row.appendChild(box)
-        }
-        board.appendChild(row)
-    }
-}
-
-initBoard()
-
 let word_chars = word.split('');
 console.log(word_chars);
 let guess = "esmed";

107dc62d877e2347ec2879edace16ce60aff6020
rit roy <ritroy@rits-MacBook-Air.local>
2022-03-23 21:00:13 -0400

    yordle displays well now

--- app/static/js/yordlestyle.js
+++ app/static/js/yordlestyle.js
@@ -3,6 +3,7 @@ let Guesses = 6;
 let Champ;
 let NumOfChamp;
 let NumOfGuessChamp;
+let p = 0;
 let py_data = fetch('/champget')
 .then(function(response){
   return response.json();
@@ -23,45 +24,72 @@ function getChamp() {
 function checkChamp(e) {
   if(e.key == "Enter" && validChamp()){
     Guesses--;
-    if(Guesses < 0) {
-      console.log("Too Bad So Sad, You Inted");
+    if(Guesses <= 0) {
+      g1.innerHTML = "Too bad, so sad, you inted";
+      g2.innerHTML = "The correct champion was: " + FullList[NumOfChamp][0];
+      g3.innerHTML = "";
+      g4.innerHTML = "";
+      g5.innerHTML = "";
+      g6.innerHTML = "";
     } else {
+      let info = '';
+      g0.innerHTML = '';
       console.log(Guesses + " Guesses Remaining");
       //Name Check
       if(FullList[NumOfGuessChamp][0] == FullList[NumOfChamp][0]) {
-        console.log("Correct Champion");
+        info += FullList[NumOfGuessChamp][0] + ": Correct Champion, ";
       } else {
-        console.log("Wrong Champion");
+        info += "Wrong Champion, ";
       }
       //Class
       if(FullList[NumOfGuessChamp][1] == FullList[NumOfChamp][1]) {
-        console.log("Correct Class");
+        info += "Correct Class, ";
       } else {
-        console.log("Wrong Class");
+        info += "Wrong Class, ";
       }
       //Region
       if(FullList[NumOfGuessChamp][2] == FullList[NumOfChamp][2]) {
-        console.log("Correct Region");
+        info += "Correct Region, ";
       } else {
-        console.log("Wrong Region");
+        info += "Wrong Region, ";
       }
       //BE
       if(FullList[NumOfGuessChamp][3] == FullList[NumOfChamp][3]) {
-        console.log("Correct Blue Essence Cost");
+        info += "Correct BE cost, ";
       } else if(parseInt(FullList[NumOfGuessChamp][3]) > parseInt(FullList[NumOfChamp][3])) {
-        console.log("Too Expensive");
+        info += "Too Expensive, ";
       } else {
-        console.log("Too Cheap");
+        info += "Too Cheap, ";
       }
       //BE
       if(FullList[NumOfGuessChamp][4] == FullList[NumOfChamp][4]) {
-        console.log("Correct Role");
+        info += "Correct Role";
       } else {
-        console.log("Wrong Role");
+        info += "Wrong Role";
       }
+      if(p == 0){
+        g1.innerHTML = "1: " + info;
+      }
+      else if(p==1){
+        g2.innerHTML = "2: " + info;
+      }
+      else if(p==2){
+        g3.innerHTML = "3: " + info;
+      }
+      else if(p==3){
+        g4.innerHTML = "4: " + info;
+      }
+      else if(p==4){
+        g5.innerHTML = "5: " + info;
+      }
+      else if(p==5){
+        g6.innerHTML = "6: " + info;
+      }
+      console.log(info);
+      p+=1;
     }
   } else {
-    console.log("That isnt a champion");
+    g0.innerHTML = "Invalid Champion";
   }
 }
 
